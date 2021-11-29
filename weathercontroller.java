8d4e947947bca11a5b729244cddb2f9549df5a3b
Samantha Helene C <91547802+IAmSamHeleneC@users.noreply.github.com>
2021-11-29 08:38:22 -0500

    Update weathercontroller.java

--- weathercontroller.java
+++ weathercontroller.java
@@ -13,4 +13,20 @@ public class WeatherController {
     public String getIndex(Model Model)
     Response response = weatherService.getForecast ("4328")
     model.addAttribute("data", response) 
+       
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/")
+    public String getIndex(Model model) {
+        model.addAttribute("request", new Request());
+        return "index";
+    }
+
+    @PostMapping
+    public String postIndex(Request request, Model model) {
+        Response data = weatherService.getForecast(request.getZipCode());
+        model.addAttribute("data", data);
+
+        return "index";
 }
