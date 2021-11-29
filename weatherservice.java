c4ed844d1062c088eebd1ce350edad6e3aec5aa2
Samantha Helene C <91547802+IAmSamHeleneC@users.noreply.github.com>
2021-11-29 08:20:40 -0500

    Create weatherservice.java

---
+++ weatherservice.java
@@ -0,0 +1,19 @@
+package com.tts.openweather.service;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+    
+@Value ("${api_key}")
+private String apiKey;
+
+public Response getForecast(String zipCode) {
+    String url = "http://api.openweathermap.org/data/2.5/weather?zip=" + 
+        zipCode + "&units=imperial&appid=" + apiKey;
+    RestTemplate restTemplate = new RestTemplate();
+    return restTemplate.getForObject(url, Response.class);
+}
+}
